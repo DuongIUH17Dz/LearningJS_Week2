1324f11a9e5e162709748fc9361f12de1e588194
H82M19\Student <Student>
2024-09-08 09:45:09 +0700

    caculature the average

--- Part2_Ex1.js
+++ Part2_Ex1.js
@@ -1,2 +1,12 @@
 //21132261_NguyenDinhDuong
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+const dolphinsScores1 = [44, 23, 71];
+const koalasScores1 = [65, 54, 49];
+
+const dolphinsScores2 = [85, 54, 41];
+const koalasScores2 = [23, 34, 27];
+const avgDolphins1 = calcAverage(...dolphinsScores1);
+const avgKoalas1 = calcAverage(...koalasScores1);
+
+const avgDolphins2 = calcAverage(...dolphinsScores2);
+const avgKoalas2 = calcAverage(...koalasScores2);
