1b3a999e1530c233a6ae435e23cdf57fc78d9aa4
H82M19\Student <Student>
2024-09-08 10:35:23 +0700

    DS_Ex1 test data

--- developer skills and edit setup/Ex1.js
+++ developer skills and edit setup/Ex1.js
@@ -13,3 +13,11 @@ const printForecast = (arr) => {
     // Join all the strings in forecastStrings with a space and print the result
     console.log(forecastStrings.join(' '));
 };
+// Test data 1
+const data1 = [17, 21, 23];
+printForecast(data1);
+// Expected output: "... 17oC in 1 days ... 21oC in 2 days ... 23oC in 3 days"
+
+// Test data 2
+const data2 = [12, 5, -5, 0, 4];
+printForecast(data2);
