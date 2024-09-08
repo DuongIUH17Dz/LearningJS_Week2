36a7dac4e36c6939bea40d5933405ba4c5c38eee
H82M19\Student <Student>
2024-09-08 10:07:48 +0700

    Ex3_create john's object

--- Part2_Ex3.js
+++ Part2_Ex3.js
@@ -7,3 +7,12 @@ const mark = {
         return this.BMI;
     }
 };
+const john = {
+    fullName: 'John Smith',
+    mass: 92, // in kg
+    height: 1.95, // in meters
+    calcBMI: function() {
+        this.BMI = this.mass / (this.height ** 2);
+        return this.BMI;
+    }
+};
